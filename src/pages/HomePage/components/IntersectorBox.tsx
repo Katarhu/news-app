9d1d41cc76fe49781cb5f5bdf81f727038d96295
Katarhu <mkaimg1@gmail.com>
2023-01-25 00:09:48 +0200

    Console logs remove

--- src/pages/HomePage/components/IntersectorBox.tsx
+++ src/pages/HomePage/components/IntersectorBox.tsx
@@ -25,7 +25,6 @@ function IntersectionBox() {
     const start = useAppSelector(selectArticlesStart);
 
     useEffect(() => {
-        console.log(articlesFilter);
         if( !inView || !!articlesFilter || articlesError ) return;
         dispatch(increaseArticlesFetchStart());
         dispatch(getArticles({ start }));
